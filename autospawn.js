1b67a0448fafeeeeac54b05166cedd15c2a52cf8
DuPont9029 <112567672+DuPont9029@users.noreply.github.com>
2024-08-31 22:49:23 +0200

    chore: Refactor autospawn function for better code organization

--- autospawn.js
+++ autospawn.js
@@ -3,35 +3,34 @@ function autospawn(number, role, spawnName) {
     var EntityNumber = _.filter(Game.creeps, (creep) => creep.memory.role == role).length; // number of creeps
     console.log('Creeps: ' + EntityNumber); 
 
-
-    if (role == "harvester" && EntityNumber < number) {
-        for (let i = 0; i < number; i++) {
-            Game.spawns[spawnName].spawnCreep([WORK, CARRY, MOVE], 'Harvester' + Game.time, { memory: { role: 'harvester' } });
+    if (role == "harvester") {
+        if (EntityNumber < number) {
+            for (let i = 0; i < number; i++) {
+                Game.spawns[spawnName].spawnCreep([WORK, CARRY, MOVE], 'Harvester' + Game.time, { memory: { role: 'harvester' } });
+            }
+        } else {
+            console.log('max number of harvester reached');
+        }
+    } 
+    else if (role == "upgrader") {
+        if (EntityNumber < number) {
+            for (let i = 0; i < number; i++) {
+                Game.spawns[spawnName].spawnCreep([WORK, CARRY, MOVE], 'Upgrader' + Game.time, { memory: { role: 'upgrader' } });
+            }
+        } else {
+            console.log('max number of upgrader reached');
+        }
+    } 
+    else if (role == "builder") {
+        if (EntityNumber < number) {
+            for (let i = 0; i < number; i++) {
+                Game.spawns[spawnName].spawnCreep([WORK, CARRY, MOVE], 'Builder' + Game.time, { memory: { role: 'builder' } });
+            }
+        } else {
+            console.log('max number of builder reached');
         }
     }
-    else {
-        console.log('max number of harvester reached');
-        
-    }
-    if (role == "upgrader" && EntityNumber < number) {
-        for (let i = 0; i < number; i++) {
-            Game.spawns[spawnName].spawnCreep([WORK, CARRY, MOVE], 'Upgrader' + Game.time, { memory: { role: 'upgrader' } });
-        }
-    }
-    else {
-        console.log('max number of upgrader reached');
-        
-    }
-    if (role == "builder" && EntityNumber < number) {
-        for (let i = 0; i < number; i++) {
-            Game.spawns[spawnName].spawnCreep([WORK, CARRY, MOVE], 'Builder' + Game.time, { memory: { role: 'builder' } });
-        }
-    }
-    else {
-        console.log('max number of builder reached');
-        
-    }
-
+    
     if (Game.spawns[spawnName].spawning) {
         var spawningCreep = Game.creeps[Game.spawns[spawnName].spawning.name];
         Game.spawns[spawnName].room.visual.text(
@@ -39,6 +38,10 @@ function autospawn(number, role, spawnName) {
             Game.spawns[spawnName].pos.x + 1,
             Game.spawns[spawnName].pos.y,
             { align: 'left', opacity: 0.8 });
+    }
+    
+    module.exports = {
+        autospawn
     }
 }
 
