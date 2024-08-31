67c42ddc5ef1d8a33a0bc53c9244414ede9eecfe
DuPont9029 <112567672+DuPont9029@users.noreply.github.com>
2024-09-01 01:08:53 +0200

    added primitive attack I.A

--- role.rangedAttacker.js
+++ role.rangedAttacker.js
@@ -1,20 +1,37 @@
+let troopnumber = 3;
+
 var roleRangedAttacker = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
+        // Posizione di pattuglia
+        var patrolFlag = Game.flags['Patrol'];
+
+        if (patrolFlag) {
+            // Conta il numero di attaccanti nella posizione di pattuglia
+            var attackersAtPatrol = _.filter(Game.creeps, (c) => c.memory.role == 'rangedAttacker' && c.pos.inRangeTo(patrolFlag.pos, 1)).length;
+
+            if (attackersAtPatrol < troopnumber && !creep.memory.readyToAttack) {
+                // Se ci sono meno di 3 attaccanti e il creep non è pronto per attaccare, muoviti verso la posizione di pattuglia
+                creep.moveTo(patrolFlag, {visualizePathStyle: {stroke: '#00ff00'}});
+                return; // Esci dalla funzione finché non ci sono abbastanza attaccanti
+            } else {
+                // Se ci sono abbastanza attaccanti, imposta il creep come pronto per attaccare
+                creep.memory.readyToAttack = true;
+            }
+        }
 
         // Cerca i nemici nella stanza
         var target = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
 
-        if(target) {
-            // Se c'è un nemico, attaccalo
+        if(target && creep.memory.readyToAttack) {
+            // Se c'è un nemico e il creep è pronto per attaccare, attaccalo
             if(creep.rangedAttack(target) == ERR_NOT_IN_RANGE) {
                 // Se il nemico non è a portata, muoviti verso di lui
                 creep.moveTo(target, {visualizePathStyle: {stroke: '#ff0000'}});
             }
-        } else {
-            // Se non ci sono nemici, muoviti verso una posizione di pattuglia
-            var patrolFlag = Game.flags['Patrol'];
+        } else if (!creep.memory.readyToAttack) {
+            // Se non ci sono nemici o il creep non è pronto per attaccare, muoviti verso una posizione di pattuglia
             if(patrolFlag) {
                 creep.moveTo(patrolFlag, {visualizePathStyle: {stroke: '#00ff00'}});
             }
