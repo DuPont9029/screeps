c723b4c5369962036c27fe613575cf2e40bff739
David Novelli <112567672+DuPont9029@users.noreply.github.com>
2024-09-02 16:53:02 +0200

    Update role.builder.js

--- role.builder.js
+++ role.builder.js
@@ -1,47 +1,60 @@
 var roleBuilder = {
 
-    /** @param {Creep} creep **/
-    run: function(creep) {
+    run: function (creep) {
 
-        if(creep.memory.building && creep.store[RESOURCE_ENERGY] == 0) {
-            creep.memory.building = false;
-            creep.memory.sourceId = null; // Resetta la fonte di risorse
-            creep.say('🔄 harvest');
+        let targets;
+        let room_dest;
+        let const_sites = creep.room.find(FIND_CONSTRUCTION_SITES)
+        let const_sites_sorted = _.sortBy(const_sites, s => s.progress)
+        
+        if (const_sites_sorted.length > 0) {
+            targets = creep.pos.findClosestByPath(const_sites_sorted)
         }
-        if(!creep.memory.building && creep.store.getFreeCapacity() == 0) {
-            creep.memory.building = true;
-            creep.say('🚧 build');
-        }
-
-        if(creep.memory.building) {
-            var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-            if(targets.length) {
-                if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+        
+        let targetEnergy = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+             filter: (structure) => {
+                return (structure.structureType === STRUCTURE_LINK || structure.structureType === STRUCTURE_TOWER
+                    || structure.structureType === STRUCTURE_SPAWN || structure.structureType === STRUCTURE_EXTENSION)
+                    && structure.store.getUsedCapacity([RESOURCE_ENERGY]) < structure.store.getCapacity([RESOURCE_ENERGY]);
                 }
-            }
+        });
+
+        if (creep.memory.room_dest != null && creep.room.name !== creep.memory.room_dest) {
+            room_dest = creep.memory.room_dest;
+            var roomName = String(room_dest);
+            creep.moveTo(new RoomPosition(25, 25, roomName));
         } else {
-            if (!creep.memory.sourceId) {
-                var sources = creep.room.find(FIND_SOURCES);
-                var targetSource = sources.find(source => {
-                    var creepsAtSource = _.filter(Game.creeps, (c) => c.memory.sourceId == source.id);
-                    var enemiesNearSource = source.pos.findInRange(FIND_HOSTILE_CREEPS, 3);
-                    return creepsAtSource.length < 3 && enemiesNearSource.length == 0;
-                });
-
-                if (targetSource) {
-                    creep.memory.sourceId = targetSource.id;
-                }
+            if (creep.memory.building && creep.store.getUsedCapacity([RESOURCE_ENERGY]) === 0) {
+                creep.memory.building = false;
+            }
+            if (!creep.memory.building && creep.store.getUsedCapacity([RESOURCE_ENERGY]) === creep.store.getCapacity([RESOURCE_ENERGY])) {
+                creep.memory.building = true;
             }
 
-            var source = Game.getObjectById(creep.memory.sourceId);
-            if (source) {
-                if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}});
+            if (creep.memory.building) {
+                if (targets !== undefined) {
+                    creep.say("⛏")
+                    if (creep.build(targets) === ERR_NOT_IN_RANGE) {
+                        creep.moveTo(targets, {visualizePathStyle: {stroke: '#ffffff'}});
+                    }
+                } else if (targetEnergy) {
+                    if (creep.transfer(targetEnergy, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+                        creep.moveTo(targetEnergy, {visualizePathStyle: {stroke: '#ffffff'}});
+                    }
                 }
             } else {
-                // Se non ci sono fonti libere e sicure, muoviti verso una posizione di attesa
-                creep.moveTo(Game.flags['WaitFlag'], {visualizePathStyle: {stroke: '#ffaa00'}});
+                var sources = creep.pos.findClosestByPath(FIND_SOURCES);
+
+                if ((creep.room.energyAvailable > 300 && targets && creep.room.storage !== undefined) && creep.room.storage.store[RESOURCE_ENERGY] > 0) {
+                    if (creep.withdraw(creep.room.storage, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+                        creep.moveTo(creep.room.storage, {visualizePathStyle: {stroke: '#ffaa00'}});
+                    }
+                } else {
+                    creep.say("⛏")
+                    if (creep.harvest(sources) === ERR_NOT_IN_RANGE) {
+                        creep.moveTo(sources, {visualizePathStyle: {stroke: '#ffaa00'}});
+                    }
+                }
             }
         }
     }
