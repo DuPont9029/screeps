7429f0074800c171ecf20cf3d882b723ce924a51
DuPont9029 <112567672+DuPont9029@users.noreply.github.com>
2024-09-04 12:44:08 +0200

    Refactor generatePixels function to remove unnecessary condition

--- generatePixels.js
+++ generatePixels.js
@@ -1,7 +1,7 @@
 function generatePixels() {
     const CPU_THRESHOLD = 5000; // Soglia di CPU necessaria per generare un pixel
 
-    if (Game.cpu.bucket >= CPU_THRESHOLD && control===true) {
+    if (Game.cpu.bucket >= CPU_THRESHOLD) {
         Game.cpu.generatePixel();
         console.log('Pixel generated. current cpu in cpu bucket: ' + Game.cpu.bucket);
     }
