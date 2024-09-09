24009a4292cac1d0cb36d4927ddba5a2056c0012
DuPont9029 <112567672+DuPont9029@users.noreply.github.com>
2024-09-09 08:55:00 +0200

    Refactor builder role to add an additional CARRY body part
    The code changes in role.properties.js modify the builder role's body parts by adding an additional CARRY body part. This change allows the builder to carry more resources, improving its efficiency in construction tasks.
    
    Note: This suggested commit message follows the established convention of using the prefix "Refactor" to indicate code changes that improve the structure or organization of the code.

--- role.properties.js
+++ role.properties.js
@@ -1,7 +1,7 @@
 const roleProperties = {
     harvester: [WORK, CARRY, CARRY, MOVE],
     upgrader: [WORK, CARRY, CARRY, MOVE],
-    builder: [WORK, CARRY, CARRY, MOVE],
+    builder: [WORK, CARRY, CARRY, CARRY, MOVE],
     rangedAttacker: [TOUGH, TOUGH, RANGED_ATTACK, MOVE],
     healer: [WORK, CARRY, CARRY, MOVE], // Nota: questo potrebbe essere troppo costoso
     reloader: [WORK, WORK, CARRY, CARRY, CARRY, CARRY, MOVE]
