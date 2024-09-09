e9ed8de16397e33ee0c41680a74936b02f86837a
DuPont9029 <112567672+DuPont9029@users.noreply.github.com>
2024-09-09 08:08:48 +0200

    Refactor harvester role to use store.getFreeCapacity()

--- role.harvester.js
+++ role.harvester.js
@@ -2,7 +2,25 @@ var roleHarvester = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-        if(creep.store.getFreeCapacity() > 0) {
+        if (creep.store.getFreeCapacity() > 0) {
+            // Se il creep ha almeno 50 di energia, trasferiscila al nucleo o a un'estensione
+            if (creep.store[RESOURCE_ENERGY] >= 50) {
+                var targets = creep.room.find(FIND_STRUCTURES, {
+                    filter: (structure) => {
+                        return (structure.structureType == STRUCTURE_EXTENSION ||
+                                structure.structureType == STRUCTURE_SPAWN) &&
+                                structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+                    }
+                });
+
+                if (targets.length > 0) {
+                    if (creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                    }
+                    return;
+                }
+            }
+
             // Se il creep non ha un nodo minerario assegnato, assegnagliene uno
             if (!creep.memory.sourceId) {
                 var sources = creep.room.find(FIND_SOURCES);
@@ -30,19 +48,21 @@ var roleHarvester = {
             // Recupera il nodo minerario assegnato
             var source = Game.getObjectById(creep.memory.sourceId);
 
-            if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
+            if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}});
             }
-        }
-        else {
+        } else {
+            // Se il creep è pieno, trasferisci l'energia al nucleo o a un'estensione
             var targets = creep.room.find(FIND_STRUCTURES, {
                 filter: (structure) => {
-                    return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
-                        structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+                    return (structure.structureType == STRUCTURE_EXTENSION ||
+                            structure.structureType == STRUCTURE_SPAWN) &&
+                            structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
                 }
             });
-            if(targets.length > 0) {
-                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+
+            if (targets.length > 0) {
+                if (creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
                 }
             }
