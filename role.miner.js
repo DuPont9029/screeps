7dbf0371fd95f15954e0458560f2c8691baca4e1
DuPont9029 <112567672+DuPont9029@users.noreply.github.com>
2024-10-13 15:29:58 +0200

    .

--- role.miner.js
+++ role.miner.js
@@ -3,37 +3,44 @@ var roleMiner = {
         // Se il creep è in modalità di raccolta e il suo inventario è pieno, passa alla modalità di deposito
         if (creep.memory.mining && creep.store.getFreeCapacity() === 0) {
             creep.memory.mining = false;
+            delete creep.memory.mineralId;
         }
         // Se il creep è in modalità di deposito e il suo inventario è vuoto, passa alla modalità di raccolta
         if (!creep.memory.mining && creep.store.getUsedCapacity() === 0) {
             creep.memory.mining = true;
+            delete creep.memory.terminalId;
         }
 
         if (creep.memory.mining) {
-            // Trova l'extractor più vicino che non è occupato
-            var extractor = creep.pos.findClosestByPath(FIND_STRUCTURES, {
-                filter: (structure) => {
-                    return structure.structureType === STRUCTURE_EXTRACTOR;
+            if (!creep.memory.mineralId) {
+                // Trova il deposito minerale più vicino
+                var mineral = creep.pos.findClosestByPath(FIND_MINERALS);
+                if (mineral) {
+                    creep.memory.mineralId = mineral.id;
                 }
-            });
+            }
 
-            // Trova il deposito minerale più vicino
-            var mineral = creep.pos.findClosestByPath(FIND_MINERALS);
-
-            if (extractor && mineral) {
+            var mineral = Game.getObjectById(creep.memory.mineralId);
+            if (mineral) {
                 if (creep.harvest(mineral) === ERR_NOT_IN_RANGE) {
                     creep.moveTo(mineral, {visualizePathStyle: {stroke: '#ffaa00'}});
                 }
             }
         } else {
-            // Trova il terminale più vicino
-            var terminal = creep.pos.findClosestByPath(FIND_STRUCTURES, {
-                filter: (structure) => {
-                    return structure.structureType === STRUCTURE_TERMINAL && 
-                           structure.store.getFreeCapacity(RESOURCE_MINERAL) > 0;
+            if (!creep.memory.terminalId) {
+                // Trova il terminale più vicino
+                var terminal = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+                    filter: (structure) => {
+                        return structure.structureType === STRUCTURE_TERMINAL && 
+                               structure.store.getFreeCapacity() > 0;
+                    }
+                });
+                if (terminal) {
+                    creep.memory.terminalId = terminal.id;
                 }
-            });
+            }
 
+            var terminal = Game.getObjectById(creep.memory.terminalId);
             if (terminal) {
                 for (const resourceType in creep.store) {
                     if (creep.transfer(terminal, resourceType) === ERR_NOT_IN_RANGE) {
