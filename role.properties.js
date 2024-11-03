0a5517bff886db732ba059d7ed846e2aa310c2f5
DuPont9029 <112567672+DuPont9029@users.noreply.github.com>
2024-11-03 15:48:39 +0100

    Refactor role.properties.js to fix regenerator role
    This commit fixes the regenerator role in the role.properties.js file. The previous code had an incorrect number of CARRY parts for the regenerator role. This fix ensures that the regenerator role has the correct number of CARRY parts, improving the functionality of the codebase.
    
    Note: This suggested commit message follows the established convention of using the prefix "Refactor" to indicate code changes that improve the structure or organization of the code.

--- role.properties.js
+++ role.properties.js
@@ -20,7 +20,7 @@ const roleProperties = {
     roomAttacker: [CLAIM, MOVE],
     drainer: [TOUGH, TOUGH,  HEAL, HEAL, MOVE, MOVE],
     distantMiner: [WORK, WORK, CARRY, CARRY, MOVE, MOVE, MOVE],
-    regenerator: [CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, MOVE]
+    regenerator: [CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, MOVE]
 };
 
 module.exports = roleProperties;
