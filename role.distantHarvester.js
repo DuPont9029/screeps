0c9fc568201bb8e3b0d8b7e26460f7189b26a699
DuPont9029 <112567672+DuPont9029@users.noreply.github.com>
2025-01-14 23:56:15 +0100

    distant harvester

--- role.distantHarvester.js
+++ role.distantHarvester.js
@@ -1,7 +1,7 @@
 var roleDistantHarvester = {
     run: function(creep) {
         // Step 1: Check for target flag
-        var targetFlag = Game.flags['HarvestFlag'];
+        var targetFlag = Game.flags['harvestFlag'];
         if (!targetFlag) {
             console.log('No target flag found');
             return;
@@ -9,7 +9,15 @@ var roleDistantHarvester = {
 
         // Step 2: Move to target room if not already there
         if (creep.room.name !== targetFlag.pos.roomName) {
+            console.log(`${creep.name} moving to target flag`);
             creep.moveTo(targetFlag, {visualizePathStyle: {stroke: '#ffaa00'}});
+            return;
+        }
+
+        // Move towards the center of the room if near the border
+        if (creep.pos.x === 0 || creep.pos.x === 49 || creep.pos.y === 0 || creep.pos.y === 49) {
+            console.log(`${creep.name} moving to center of the room`);
+            creep.moveTo(25, 25);
             return;
         }
 
@@ -26,20 +34,42 @@ var roleDistantHarvester = {
         });
 
         if (containers.length === 0) {
+            console.log(`${creep.name} no container found near source, checking for construction sites`);
             // Step 5: Build container if none exists
             var constructionSites = source.pos.findInRange(FIND_CONSTRUCTION_SITES, 1, {
                 filter: (site) => site.structureType === STRUCTURE_CONTAINER
             });
 
             if (constructionSites.length === 0) {
-                creep.room.createConstructionSite(source.pos.x, source.pos.y + 1, STRUCTURE_CONTAINER);
+                console.log(`${creep.name} no construction site found, creating one`);
+                // Find a free spot around the source to place the container
+                var positions = [
+                    {x: source.pos.x - 1, y: source.pos.y},
+                    {x: source.pos.x + 1, y: source.pos.y},
+                    {x: source.pos.x, y: source.pos.y - 1},
+                    {x: source.pos.x, y: source.pos.y + 1},
+                    {x: source.pos.x - 1, y: source.pos.y - 1},
+                    {x: source.pos.x + 1, y: source.pos.y + 1},
+                    {x: source.pos.x - 1, y: source.pos.y + 1},
+                    {x: source.pos.x + 1, y: source.pos.y - 1}
+                ];
+
+                for (var pos of positions) {
+                    var terrain = creep.room.getTerrain().get(pos.x, pos.y);
+                    if (terrain !== TERRAIN_MASK_WALL) {
+                        creep.room.createConstructionSite(pos.x, pos.y, STRUCTURE_CONTAINER);
+                        break;
+                    }
+                }
             } else {
                 if (creep.store[RESOURCE_ENERGY] === 0) {
+                    console.log(`${creep.name} harvesting energy`);
                     if (creep.harvest(source) === ERR_NOT_IN_RANGE) {
                         creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}});
                     }
                 } else {
                     var constructionSite = constructionSites[0];
+                    console.log(`${creep.name} building construction site`);
                     if (creep.build(constructionSite) === ERR_NOT_IN_RANGE) {
                         creep.moveTo(constructionSite, {visualizePathStyle: {stroke: '#ffffff'}});
                     }
@@ -47,6 +77,7 @@ var roleDistantHarvester = {
             }
         } else {
             // Step 6: Check for other sources without containers
+            console.log(`${creep.name} container found, checking other sources`);
             var otherSources = creep.room.find(FIND_SOURCES, {
                 filter: (s) => s.id !== source.id
             });
@@ -62,7 +93,26 @@ var roleDistantHarvester = {
                     });
 
                     if (otherConstructionSites.length === 0) {
-                        creep.room.createConstructionSite(otherSource.pos.x, otherSource.pos.y + 1, STRUCTURE_CONTAINER);
+                        console.log(`${creep.name} creating construction site for other source`);
+                        // Find a free spot around the other source to place the container
+                        var otherPositions = [
+                            {x: otherSource.pos.x - 1, y: otherSource.pos.y},
+                            {x: otherSource.pos.x + 1, y: otherSource.pos.y},
+                            {x: otherSource.pos.x, y: otherSource.pos.y - 1},
+                            {x: otherSource.pos.x, y: otherSource.pos.y + 1},
+                            {x: otherSource.pos.x - 1, y: otherSource.pos.y - 1},
+                            {x: otherSource.pos.x + 1, y: otherSource.pos.y + 1},
+                            {x: otherSource.pos.x - 1, y: otherSource.pos.y + 1},
+                            {x: otherSource.pos.x + 1, y: otherSource.pos.y - 1}
+                        ];
+
+                        for (var otherPos of otherPositions) {
+                            var otherTerrain = creep.room.getTerrain().get(otherPos.x, otherPos.y);
+                            if (otherTerrain !== TERRAIN_MASK_WALL) {
+                                creep.room.createConstructionSite(otherPos.x, otherPos.y, STRUCTURE_CONTAINER);
+                                break;
+                            }
+                        }
                     }
                 }
             }
@@ -70,12 +120,21 @@ var roleDistantHarvester = {
             // Step 7: Mine and deposit energy in container
             var container = containers[0];
             if (creep.store.getFreeCapacity() > 0) {
+                console.log(`${creep.name} harvesting energy`);
                 if (creep.harvest(source) === ERR_NOT_IN_RANGE) {
                     creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}});
                 }
             } else {
-                if (creep.transfer(container, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
-                    creep.moveTo(container, {visualizePathStyle: {stroke: '#ffffff'}});
+                if (container.store.getFreeCapacity(RESOURCE_ENERGY) > 0) {
+                    console.log(`${creep.name} transferring energy to container`);
+                    if (creep.transfer(container, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+                        creep.moveTo(container, {visualizePathStyle: {stroke: '#ffffff'}});
+                    }
+                } else {
+                    console.log(`${creep.name} container full, repairing container`);
+                    if (creep.repair(container) === ERR_NOT_IN_RANGE) {
+                        creep.moveTo(container, {visualizePathStyle: {stroke: '#ffffff'}});
+                    }
                 }
             }
         }
